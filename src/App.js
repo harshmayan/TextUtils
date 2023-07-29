74953cafdc39a7bce3772c63558a99246cbcd794
harshmayan <harshmayani07@gmail.com>
2023-07-29 14:28:18 +0530

    changes detected

--- src/App.js
+++ src/App.js
@@ -24,13 +24,24 @@ function App() {
       setAlert(null);
     }, 1500);
   }
+  // const removeBodyClasses = ()=>{
+  //   document.body.classList.remove('bg-light')
+  //   document.body.classList.remove('bg-dark')
+  //   document.body.classList.remove('bg-warning')
+  //   document.body.classList.remove('bg-success')
+  //   document.body.classList.remove('bg-danger')
+  // }
 
+  // const toggleMode = (cls)=>{
   const toggleMode = ()=>{
+    // removeBodyClasses();
+    // console.log(cls)
+    // document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = 'TextUtils - Dark Mode';
+      // document.title = 'TextUtils - Dark Mode';
       // setInterval(() => {
       //   document.title = 'TextUtils is Amazing Mode';
       // }, 2000);
@@ -41,7 +52,7 @@ function App() {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = 'TextUtils - Light Mode';
+      // document.title = 'TextUtils - Light Mode';
     }
   }
   return (
@@ -56,7 +67,7 @@ function App() {
       <div className="container my-3">
       <Routes>
         <Route exact path="/about" element={<About mode={mode}/>}/>
-        <Route exact path="/" element={<TextForm  showAlert={showAlert} heading="Enter The Text To Analyze Below!!" mode={mode}/>}/>
+        <Route exact path="/" element={<TextForm  showAlert={showAlert} heading="Try TextUtils - Word Counter ,Character Counter ,Remove extra spaces" mode={mode}/>}/>
       </Routes>
       </div>
     </BrowserRouter>
